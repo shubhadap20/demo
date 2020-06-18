b4a28af9d432e1a64436fcc0edede8c6fa8d6d7c
shubhadap20 <67098409+shubhadap20@users.noreply.github.com>
2020-06-18 15:02:40 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int a;
+    printf("enter a:");
+    scanf("%d",&a);
+
+    if((a%4==0&&a%100!=0)||(a%400==0))
+        printf("a is leap year");
+    else
+        printf("a is ordinary year");
+        return 0;
+
+}
